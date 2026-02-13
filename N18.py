3563c75a096aa0f9a78d97266019cd1d6215615f
MAxhere-png <maxnwind@gmail.com>
2026-02-13 19:26:41 +0100

    Завдання18

---
+++ N18.py
@@ -0,0 +1,7 @@
+try:
+    number_float = float(input("Введіть число:"))
+    number_int = int(number_float)
+    print("int number:", number_int)
+except(ValueError):
+    print("Ці дані неможливо конвертувати в ціле число")
+    
